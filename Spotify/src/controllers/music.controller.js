77cd9f9d1d260fc6862e5f180d7453ada42a8c3a
Omkar Ghadge <omkarghadge68@gmail.com>
2026-04-23 11:14:04 +0530

    Implemented music and album creation with authentication

--- Spotify/src/controllers/music.controller.js
+++ Spotify/src/controllers/music.controller.js
@@ -1,47 +1,65 @@
 const musicModel = require('../models/music.model')
+const albumModel = require('../models/album.model')
 const { uploadFile } = require('../services/storage.service')
 const jwt = require('jsonwebtoken')
 
 async function createMusic(req, res) {
-    const token = req.cookies.token
-    if (!token) {
-        return res.status(401).json({
-            message: "Unauthorized"
-        })
-    }
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        if (decoded.role !== "artist") {
-            return res.status(403).json({
-                message: "You are not authorized to perform this action"
-            })
+    const { title } = req.body
+    const file = req.file
+
+    const result = await uploadFile(file.buffer.toString('base64'))
+
+    const music = await musicModel.create({
+        uri: result.url,
+        title,
+        artist: req.user.id
+    })
+
+    res.status(201).json({
+        message: "Message created successfully",
+        music: {
+            id: music._id,
+            uri: music.uri,
+            title: music.title,
+            artist: music.artist
         }
-        const { title } = req.body
-        const file = req.file
-
-        const result = await uploadFile(file.buffer.toString('base64'))
-
-        const music = await musicModel.create({
-            uri: result.url,
-            title,
-            artist: decoded.id
-        })
-
-        res.status(201).json({
-            message: "Message created successfully",
-            music: {
-                id: music._id,
-                uri: music.uri,
-                title: music.title,
-                artist: music.artist
-            }
-        })
-    } catch (error) {
-        console.error(error);
-        return res.status(401).json({
-            message: "Unauthorized"
-        })
-    }
+    })
 }
 
-module.exports = { createMusic }
+async function createAlbum(req, res) {
+    const { title, musics } = req.body
+    const album = await albumModel.create({
+        title,
+        artist: req.user.id,
+        musics: musics
+    })
+    res.status(201).json({
+        message: "Album created successfully",
+        album: {
+            id: album._id,
+            title: album.title,
+            artist: album.artist,
+            musics: album.musics
+        }
+    })
+}
+
+async function getAllMusics(req, res) {
+    const musics = await musicModel.find().populate("artist")
+
+    res.status(200).json({
+        message: "Music fetched successfully",
+        musics: musics
+    })
+}
+
+async function getAllAlbums(req, res) {
+    const albums = await albumModel.find().populate("artist").populate("musics")
+
+    res.status(200).json({
+        message: "Albums fetched successfully",
+        albums: albums
+    })
+}
+
+module.exports = { createMusic, createAlbum, getAllMusics, getAllAlbums }
---
+++ Spotify/src/middlewares/auth.middleware.js
@@ -0,0 +1,55 @@
+const jwt = require('jsonwebtoken')
+
+async function authArtist(req, res, next) {
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (decoded.role !== "artist") {
+            return res.status(403).json({
+                message: "You are not authorized to perform this action"
+            })
+        }
+        req.user = decoded
+
+        next()
+    } catch (error) {
+        console.error(error);
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+}
+
+async function authUser(req, res, next) {
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if (decoded.role !== "user") {
+            return res.status(403).json({
+                message: "You are not authorized to perform this action"
+            })
+        }
+        req.user = decoded
+        next()
+    }
+    catch (error) {
+        console.error(error);
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+}
+
+module.exports = { authArtist, authUser }
